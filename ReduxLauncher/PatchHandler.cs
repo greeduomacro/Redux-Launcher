0ddaf8d807725d72f3bf2b78a6de9bae30c40bb3
enroq <enroq@kairyt.com>
2015-05-17 13:22:18 -0400

    Dropping Core Structure Logic - (File Download)

--- ReduxLauncher/PatchHandler.cs
+++ ReduxLauncher/PatchHandler.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//By using or redistributing this software you are agreeing to leave
+//the entire header in-tact.
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU Affero License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU Affero License for more details.
+
+//You should have received a copy of the GNU Affero License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//Copyright (C) 2015 Redux Dev Team (uo-redux.com)
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
